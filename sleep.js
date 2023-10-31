9b03b818de64dcbfe6fe0f1998d23dfb3cc7b435
Andy Wong <103609739+Shardnasc@users.noreply.github.com>
2023-10-31 12:35:13 -0400

    Update sleep.js

--- sleep.js
+++ sleep.js
@@ -1,22 +1,35 @@
-
+//Sleep problem: https://leetcode.com/problems/sleep/
+//Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
 /**
- * @return {Function}
+ * @param {number} millis
+ * @return {Promise}
  */
-var createHelloWorld = function() {
+async function sleep(millis) {
     
-    return function(...args) {
-        
-    }
-};
+}
 
-/**
- * const f = createHelloWorld();
- * f(); // "Hello World"
+
+// Example usage:
+let t = Date.now();
+sleep(100).then((result) => {
+  console.log(result); // 100
+  console.log(Date.now() - t); // 100
+});
+
+
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
  */
 
- var createHelloWorld = function() {
-    
-    return function(...args) {
-    const SetIn
-    }
-};
+ //Using the above function sleep to solve this with sleep and millis
+//I used to return a new promise ( complete failure to reject like an error
+function sleep(millis) {
+return new Promise(resolve, reject)
+
+
+
+}
+
+//This problem stumbled me and this is how far I went. I was thinking of setTimeOut, but it needs to be resolved.
